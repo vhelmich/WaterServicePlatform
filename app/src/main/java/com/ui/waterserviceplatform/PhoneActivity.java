d8ddd0aeaa0cfb70c454be355e31540e63269b25
Jean-Marcellin Truong <jean-marcellin.truong@ensimag.grenoble-inp.fr>
2017-12-02 22:23:38 +0200

    Automatically retrieve the phone number for the confirmation
    If the user gives us the permission, the phone number is filled
    automatically.

--- app/src/main/java/com/ui/waterserviceplatform/PhoneActivity.java
+++ app/src/main/java/com/ui/waterserviceplatform/PhoneActivity.java
@@ -1,9 +1,16 @@
 package com.ui.waterserviceplatform;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,14 +23,11 @@ import android.widget.Toast;
 
 public class PhoneActivity extends AppCompatActivity {
 
-    private TextView errormsgPhone;
-    private TextView errormsgCode;
     private EditText phoneField;
     private String phoneNumber;
-    private String code;
-    private EditText codeField;
     private String generatedCode;
-
+    private Button confirmButton;
+    private final static int MY_PERMISSIONS_REQUEST_READ_SMS = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +36,7 @@ public class PhoneActivity extends AppCompatActivity {
 
         phoneField = (EditText) findViewById(R.id.phoneNumberField);
 
-
-
-
-        final Button confirmButton = findViewById(R.id.confirmNumberButton);
+        confirmButton = findViewById(R.id.confirmNumberButton);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 phoneNumber = phoneField.getText().toString();
@@ -44,7 +45,6 @@ public class PhoneActivity extends AppCompatActivity {
                     generatedCode = "123456"; //set here only for testing
                     Intent intent = new Intent(getApplicationContext(), CodeActivity.class);
                     intent.putExtra("stringname",generatedCode);
-                    System.out.println(intent.getStringExtra("stringname"));
                     startActivity(intent);
 
 
@@ -59,13 +59,58 @@ public class PhoneActivity extends AppCompatActivity {
 
             }
         });
-
-
-
-
-
-
+        requestPhonePermission();
     }
+
+    private void requestPhonePermission(){
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("Permission required");
+            alertDialog.setMessage("We can automatically verify your number if you give us the permission" +
+                    " to read your SMS, else you will have to do it manually.");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    ActivityCompat.requestPermissions(PhoneActivity.this,
+                            new String[]{Manifest.permission.READ_SMS},
+                            MY_PERMISSIONS_REQUEST_READ_SMS);
+                }
+            });
+            alertDialog.show();
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                        String number = telephonyManager.getLine1Number();
+                        phoneField.setText(number);
+                        confirmButton.performClick();
+                    }
+                    catch(SecurityException e){
+
+                    }
+                }
+                return;
+            }
+        }
+    }
+
 
     private boolean checkPhoneNumber(String phoneNumber) {
 
