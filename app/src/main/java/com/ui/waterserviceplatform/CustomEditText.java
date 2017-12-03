a095b1fdff1f3f9af4712f34488dd2a5d8df57ac
Jean-Marcellin Truong <jean-marcellin.truong@ensimag.grenoble-inp.fr>
2017-12-03 12:52:50 +0200

    Created CustomEditText
    We couldn't get the event of backspace if the edittext was empty, this
    customedittext resolves this problem

---
+++ app/src/main/java/com/ui/waterserviceplatform/CustomEditText.java
@@ -0,0 +1,80 @@
+package com.ui.waterserviceplatform;
+
+import android.support.v7.widget.AppCompatEditText;
+import java.util.Random;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputConnectionWrapper;
+
+public class CustomEditText extends AppCompatEditText {
+
+    private CustomEditText before = null;
+    private Random r = new Random();
+
+    public void setBefore(CustomEditText cet){
+        before = cet;
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomEditText(Context context) {
+        super(context);
+    }
+
+    public void setRandomBackgroundColor() {
+        setBackgroundColor(Color.rgb(r.nextInt(256), r.nextInt(256), r
+                .nextInt(256)));
+    }
+
+    @Override
+    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+        return new CustomInputConnection(super.onCreateInputConnection(outAttrs),
+                true);
+    }
+
+    private class CustomInputConnection extends InputConnectionWrapper {
+
+        public CustomInputConnection(InputConnection target, boolean mutable) {
+            super(target, mutable);
+        }
+
+        @Override
+        public boolean sendKeyEvent(KeyEvent event) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN
+                    && event.getKeyCode() == KeyEvent.KEYCODE_DEL) {
+                if(CustomEditText.this.getText().length()==0 && before != null){
+                    before.requestFocus();
+                    return false;
+                }
+            }
+            return super.sendKeyEvent(event);
+        }
+
+
+        @Override
+        public boolean deleteSurroundingText(int beforeLength, int afterLength) {
+            // magic: in latest Android, deleteSurroundingText(1, 0) will be called for backspace
+            if (beforeLength == 1 && afterLength == 0) {
+                // backspace
+                return sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL))
+                        && sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DEL));
+            }
+
+            return super.deleteSurroundingText(beforeLength, afterLength);
+        }
+
+    }
+
+}
