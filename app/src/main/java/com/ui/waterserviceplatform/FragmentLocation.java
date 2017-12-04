c19e29914e083dc363457768c9c72f35117aad79
Jean-Marcellin Truong <jean-marcellin.truong@ensimag.grenoble-inp.fr>
2017-12-04 14:20:01 +0200

    Added GPS location support

--- app/src/main/java/com/ui/waterserviceplatform/FragmentLocation.java
+++ app/src/main/java/com/ui/waterserviceplatform/FragmentLocation.java
@@ -4,19 +4,29 @@ package com.ui.waterserviceplatform;
  * Created by vhelmich on 29/11/17.
  */
 
+import android.*;
+import android.Manifest;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.places.AutocompleteFilter;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
@@ -25,7 +35,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -37,6 +46,7 @@ public class FragmentLocation extends Fragment {
     private PlaceAutocompleteFragment placeAutoComplete;
     private Marker currentMarker;
     private MapFragment mapFragment;
+    private final static int MY_PERMISSIONS_REQUEST_ACCESS_GPS = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -76,11 +86,12 @@ public class FragmentLocation extends Fragment {
             @Override
             public void onMapReady(GoogleMap mMap) {
                 googleMap = mMap;
-
-                LatLng nairobi = new LatLng(-1.2920408, 36.8256984);
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(nairobi).zoom(12).build();
-                
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                if(checkPermissionGPS()){
+                    moveToPosition();
+                }
+                else{
+                    defaultLocation();
+                }
                 addListenerLongTouch();
             }
         });
@@ -124,5 +135,103 @@ public class FragmentLocation extends Fragment {
 
             }
         });
+    }
+
+    private boolean checkPermissionGPS(){
+        boolean granted = ContextCompat.checkSelfPermission(this.getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED;
+        if (!granted){
+            ActivityCompat.requestPermissions(this.getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_GPS);
+        }
+        return granted;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_GPS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                   moveToPosition();
+                }
+                else {
+                    defaultLocation();
+                }
+                return;
+            }
+        }
+    }
+
+    private void changeButtonPosition(){
+        View mapView = mapFragment.getView();
+        if (mapView != null &&
+                mapView.findViewById(Integer.parseInt("1")) != null) {
+            View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent())
+                    .findViewById(Integer.parseInt("2"));
+            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)
+                    locationButton.getLayoutParams();
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+            layoutParams.setMargins(0, 0, 30, 30);
+        }
+        googleMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener(){
+            @Override
+            public boolean onMyLocationButtonClick()
+            {
+                LocationManager locationManager = (LocationManager)
+                        getActivity().getSystemService(Context.LOCATION_SERVICE);
+                android.location.LocationListener locationListener = new android.location.LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        addMarkerAndMove(new LatLng(location.getLatitude(), location.getLongitude()));
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+
+                    }
+                };
+                try {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            5000,
+                            10,
+                            locationListener);
+                }
+                catch(SecurityException e){
+                    
+                }
+                return false;
+            }
+        });
+    }
+
+    private void moveToPosition(){
+        try {
+            googleMap.setMyLocationEnabled(true);
+            changeButtonPosition();
+        }
+        catch(SecurityException e){
+
+        }
+    }
+
+    private void defaultLocation(){
+        LatLng nairobi = new LatLng(-1.2920408, 36.8256984);
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(nairobi).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
