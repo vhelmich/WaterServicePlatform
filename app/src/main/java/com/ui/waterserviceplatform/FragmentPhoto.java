2b6434fd3b6b0662881227f5c3a532724cb64beb
Jean-Marcellin Truong <jean-marcellin.truong@ensimag.grenoble-inp.fr>
2017-12-08 01:46:55 +0200

    Photo fragment working

--- app/src/main/java/com/ui/waterserviceplatform/FragmentPhoto.java
+++ app/src/main/java/com/ui/waterserviceplatform/FragmentPhoto.java
@@ -3,17 +3,31 @@ package com.ui.waterserviceplatform;
 /**
  * Created by vhelmich on 29/11/17.
  */
+import android.*;
+import android.Manifest;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.util.ArrayList;
+
+import gun0912.tedbottompicker.TedBottomPicker;
 
 public class FragmentPhoto extends Fragment {
 
-        public FragmentPhoto() {
-            // Required empty public constructor
-        }
+        private TedBottomPicker bottomSheet;
+        private GridView gv;
+        private ImageAdapter imageAdapter;
+        private FloatingActionButton button;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -23,9 +37,65 @@ public class FragmentPhoto extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_photo, container, false);
+            View view = inflater.inflate(R.layout.fragment_photo, container, false);
+            checkStoragePermission();
+
+            button = (FloatingActionButton) view.findViewById(R.id.pickImage);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    createBottomSheet();
+                    bottomSheet.show(getActivity().getSupportFragmentManager());
+                }
+            });
+
+            gv = (GridView) view.findViewById(R.id.gridview);
+            imageAdapter = new ImageAdapter(this, getActivity().getApplicationContext());
+            gv.setAdapter(imageAdapter);
+
+            return view;
         }
 
+        private void checkStoragePermission(){
+            PermissionListener permissionlistener = new PermissionListener() {
+                @Override
+                public void onPermissionGranted() {
+                }
+
+                @Override
+                public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                }
+            };
+            TedPermission.with(getContext())
+                    .setPermissionListener(permissionlistener)
+                    .setRationaleTitle("Requesting permission")
+                    .setRationaleMessage("We need to get access to your storage to send pictures.")
+                    .setDeniedMessage("Permission denied")
+                    .setDeniedMessage("If you reject permission, you cannot send pictures.\n" +
+                            "Please turn on permissions at [Setting] > [Permission]")
+                    .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    .check();
+        }
+
+        private void createBottomSheet(){
+            bottomSheet = new TedBottomPicker.Builder(getContext())
+                    .setOnMultiImageSelectedListener(new TedBottomPicker.OnMultiImageSelectedListener() {
+                        @Override
+                        public void onImagesSelected(ArrayList<Uri> uriList) {
+                            imageAdapter.addImages(uriList);
+                        }
+                    })
+                    .setPeekHeight(1300)
+                    .showTitle(false)
+                    .setSelectMinCount(1)
+                    .setSelectMaxCount(3-imageAdapter.getCount())
+                    .setCompleteButtonText("Done")
+                    .setEmptySelectionText("No Select")
+                    .create();
+        }
+
+        public void setButtonVisiblity(int visible){
+            button.setVisibility(visible);
+        }
 
 }
---
+++ app/src/main/java/com/ui/waterserviceplatform/ImageAdapter.java
@@ -0,0 +1,95 @@
+package com.ui.waterserviceplatform;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.Size;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageSize;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jm on 12/7/17.
+ */
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+    private ArrayList<Uri> imagesUri = new ArrayList<>();
+    private DisplayImageOptions options;
+    private ImageLoader imageLoader;
+    private FragmentPhoto fp;
+
+    public ImageAdapter(FragmentPhoto f, Context c) {
+        mContext = c;
+        fp=f;
+        options = new DisplayImageOptions.Builder().build();
+        imageLoader = ImageLoader.getInstance();
+        imageLoader.init(new ImageLoaderConfiguration.Builder(c).build());
+
+    }
+
+    public int getCount() {
+        return imagesUri.size();
+    }
+
+    public Object getItem(int position) {
+        return imagesUri.get(position);
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final Uri image = imagesUri.get(position);
+
+        if (convertView == null) {
+            final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.image_layout, null);
+        }
+        final ImageView imageView = (ImageView)convertView.findViewById(R.id.image_grid);
+        final ImageView close = (ImageView)convertView.findViewById(R.id.image_delete);
+        ImageSize targetSize = new ImageSize(200,200);
+        Bitmap bmp = imageLoader.loadImageSync(image.toString(),  targetSize,options);
+        Bitmap resized = Bitmap.createScaledBitmap(bmp, 200, 200, true);
+        imageView.setImageBitmap(resized);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeImage(position);
+                fp.setButtonVisiblity(View.VISIBLE);
+            }
+        });
+        return convertView;
+    }
+
+    private void removeImage(int position){
+        imagesUri.remove(position);
+        this.notifyDataSetChanged();
+    }
+
+    public void addImages(ArrayList<Uri> images){
+        imagesUri.addAll(images);
+        if(this.getCount()==3){
+            fp.setButtonVisiblity(View.INVISIBLE);
+        }
+        this.notifyDataSetChanged();
+    }
+}
