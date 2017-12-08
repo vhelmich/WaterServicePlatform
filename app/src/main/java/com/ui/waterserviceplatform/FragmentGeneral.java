071b28fcea394c8f403f478e72f3c9de95d31e7f
Jean-Marcellin Truong <jean-marcellin.truong@ensimag.grenoble-inp.fr>
2017-12-08 10:43:01 +0200

    Added scrollView in FragmentGeneral
    Before the content was hidde nif the keyboard popped

--- app/src/main/java/com/ui/waterserviceplatform/FragmentGeneral.java
+++ app/src/main/java/com/ui/waterserviceplatform/FragmentGeneral.java
@@ -7,6 +7,7 @@ package com.ui.waterserviceplatform;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentContainer;
 import android.support.v4.app.FragmentManager;
@@ -16,7 +17,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.Button;
 
@@ -26,6 +29,7 @@ public class FragmentGeneral extends Fragment {
     private static final int RANGE = 10;
     private static final int STEP_SIZE = 1;
     private EditText idField, addInfoField;
+    private ScrollView scrollview;
 
     public int intensity;
     public String pipeID;
@@ -55,7 +59,24 @@ public class FragmentGeneral extends Fragment {
         slider = (SeekBar) view.findViewById(R.id.seekbarDamage);
         idField = (EditText) view.findViewById(R.id.idPipe);
         addInfoField = (EditText) view.findViewById(R.id.addInfo);
+        scrollview = (ScrollView) view.findViewById(R.id.scrollview);
 
+        addInfoField.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    new Handler().postDelayed(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            scrollview.fullScroll(View.FOCUS_DOWN);
+                        }
+
+                    }, 500);
+
+                }
+            }
+        });
         slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -75,11 +96,20 @@ public class FragmentGeneral extends Fragment {
         nextButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 saveContent();
+                hideKeyboard();
                 ((MainActivity)getActivity()).switchTab(1);
             }
         });
 
         return view;
+    }
+
+    private void hideKeyboard(){
+        InputMethodManager inputManager = (InputMethodManager)
+                getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
     }
 
     public void saveContent() {
