a6c907c8354b96386d2ee8b2e16661984e1428ac
Jean-Marcellin Truong <jean-marcellin.truong@ensimag.grenoble-inp.fr>
2017-12-09 02:15:56 +0200

    Cleaned CustomEditText

--- app/src/main/java/com/ui/waterserviceplatform/CustomEditText.java
+++ app/src/main/java/com/ui/waterserviceplatform/CustomEditText.java
@@ -6,7 +6,6 @@ import java.util.Random;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
@@ -32,12 +31,7 @@ public class CustomEditText extends AppCompatEditText {
     public CustomEditText(Context context) {
         super(context);
     }
-
-    public void setRandomBackgroundColor() {
-        setBackgroundColor(Color.rgb(r.nextInt(256), r.nextInt(256), r
-                .nextInt(256)));
-    }
-
+    
     @Override
     public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
         return new CustomInputConnection(super.onCreateInputConnection(outAttrs),
